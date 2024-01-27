bcc0ea6d5537a7a5c2bb81a724069f2b8de28627
Xavier Clavel <xrclavel@gmail.com>
2024-01-27 10:53:53 +0100

    removed methods

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -38,15 +38,4 @@ public class Player : MonoBehaviour
        };
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
