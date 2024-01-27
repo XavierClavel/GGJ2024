a3ad0171083656322f7068a50848b090c555fc89
Xavier Clavel <xrclavel@gmail.com>
2024-01-27 11:53:37 +0100

    updated recipe retrieval

--- Assets/Scripts/RecipeManager.cs
+++ Assets/Scripts/RecipeManager.cs
@@ -6,14 +6,39 @@ public static class RecipeManager
 {
    private static List<Recipe> recipes;
 
+   public static Recipe findRecipe(string key)
+   {
+      foreach (var recipe in recipes)
+      {
+         if (recipe.matchesInput(key)) return recipe;
+      }
+
+      return null;
+   }
+
    public static Recipe getRecipe(List<string> cardKeys)
    {
+      //Look for exact recipe
       foreach (var recipe in recipes)
       {
          if (recipe.matchesInput(cardKeys)) return recipe;
       }
+      
+      //If one card is intonation => fail
+      foreach (var cardKey in cardKeys)
+      {
+         if (DataManager.dictKeyToCard[cardKey].isIntonation()) return null;
+      }
+      
+      //Build output based on cards
+      Recipe newRecipe = new Recipe();
+      foreach (var cardKey in cardKeys)
+      {
+         newRecipe.addOutput(findRecipe(cardKey));
+      }
 
-      throw new ArgumentOutOfRangeException("Recipe not found");
+      return newRecipe;
+
    }
 
    public static Dictionary<string, int> getRecipeEmotions(List<string> cardKeys)
--- Assets/Scripts/ScriptablesObjects/CardHandler.cs
+++ Assets/Scripts/ScriptablesObjects/CardHandler.cs
@@ -6,9 +6,11 @@ public class ObjectHandler : ScriptableObject
 {
     [SerializeField] protected string key;
     [SerializeField] protected Sprite icon;
+    [SerializeField] protected bool intonation;
 
-    public string getKey() { return key.Trim(); }
-    public Sprite getIcon() { return icon; }
+    public string getKey() => key.Trim();
+    public Sprite getIcon() => icon;
+    public bool isIntonation() => intonation;
 }
 
 [CreateAssetMenu(fileName = "CardHandler", menuName = Vault.other.scriptableObjectMenu + "CardHandler", order = 0)]
