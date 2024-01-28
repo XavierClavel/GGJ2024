3df53250c2679aa59358a4ddfc19f6077395b0b2
Xavier Clavel <xrclavel@gmail.com>
2024-01-28 11:18:50 +0100

    notebook start

--- Assets/Scripts/ScriptablesObjects/IntonationHandler.cs
+++ Assets/Scripts/ScriptablesObjects/IntonationHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "IntonationHandler", menuName = Vault.other.scriptableObjectMenu + "IntonationHandler", order = 0)]
-public class IntonationHandler : ObjectHandler
+public class IntonationHandler : CardHandler
 {
     public override bool isIntonation() => true;
 }
