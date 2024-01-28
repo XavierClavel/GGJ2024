9ea82c7a738f32cde81872c77e8967b9c42f303e
Xavier Clavel <xrclavel@gmail.com>
2024-01-28 12:10:40 +0100

    notebook hide & show

--- Assets/Scripts/UI/Notebook.cs
+++ Assets/Scripts/UI/Notebook.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class Notebook : MonoBehaviour
@@ -8,6 +9,19 @@ public class Notebook : MonoBehaviour
     [SerializeField] private List<NotebookPage> pages;
     private int currentPage = 0;
     public static Notebook instance;
+    private static Vector2 posDisplayed = Vector2.zero;
+    private static Vector2 posHidden = new Vector2(-1100f, -850f);
+    [SerializeField] private RectTransform rectTransform;
+
+    public void Hide()
+    {
+        rectTransform.DOAnchorPos(posHidden, 1f).SetEase(Ease.InOutQuad);
+    }
+
+    public void Show()
+    {
+        rectTransform.DOAnchorPos(posDisplayed, 1f).SetEase(Ease.InOutQuad);
+    }
 
     private void Awake()
     {
